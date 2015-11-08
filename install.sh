b597e238b7363555613c97cfc641468eb7ca3406
wangchao <xxhank.boy@gmail.com>
2015-11-09 01:34:37 +0800

    提交心的code snipet

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ RUN_DIR=`PWD`
 function safe_ln( ){
     source=$1
     targetDir=$HOME/$2
+    mkdir -p "${targetDir}"
 
     if [[ -e "${targetDir}"/"$1" ]]; then
         mv  "${targetDir}"/"$1" "${targetDir}"/"$1"-{`date "+%Y%m%d-%H%M%S"`}.bak
@@ -18,7 +19,7 @@ function safe_ln( ){
 
 safe_ln "Plug-ins"    "/Library/Application Support/Developer/Shared/Xcode"
 safe_ln "Templates"   "/Library/Developer/Xcode" 
-safe_ln "UserData"   "/Library/Developer/Xcode" 
+safe_ln "UserData"    "/Library/Developer/Xcode" 
 
 
 #IFS=o
