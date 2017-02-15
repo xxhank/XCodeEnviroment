411bcbd23da1dd26d4ec9d4c520a6f83f4e30675
王超9 <wangchao9@le.com>
2017-02-15 15:48:49 +0800

    新的安装脚本

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
 RUN_DIR=`dirname $0`
 RUN_DIR=`PWD`
 
@@ -18,8 +19,8 @@ function safe_ln( ){
 }
 
 safe_ln "Plug-ins"    "/Library/Application Support/Developer/Shared/Xcode"
-safe_ln "Templates"   "/Library/Developer/Xcode" 
-safe_ln "UserData"    "/Library/Developer/Xcode" 
+safe_ln "Templates"   "/Library/Developer/Xcode"
+safe_ln "UserData"    "/Library/Developer/Xcode"
 
 
 #IFS=o
